5c496750a8a5df7f4aeef9895708291fafbdb258
Adib-Elang <bukandes9@gmail.com>
2025-12-01 19:47:13 +0700

    b

--- script.js
+++ script.js
@@ -28,3 +28,22 @@ window.onclick = function (event) {
         }
     }
 }
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.btn btn-outline-primary me-2')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
