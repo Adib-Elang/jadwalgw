3d96b343460c26767bb2ecc563a057c6e1c6989a
Adib-Elang <bukandes9@gmail.com>
2025-12-01 19:40:27 +0700

    mobile test

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.btn btn-outline-primary me-2')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+}
+window.onclick = function (event) {
+    if (!event.target.matches('.btn btn-outline-primary me-2')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
